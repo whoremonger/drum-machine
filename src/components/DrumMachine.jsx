a40d90ce197d7e26feaa606cbbea88998f293ce7
Jarrad L <jlovelace2500k@gmail.com>
2024-05-29 22:05:52 -0700

    Added react-materialize and DrumMachine component to set up project.

---
+++ src/components/DrumMachine.jsx
@@ -0,0 +1,20 @@
+
+//import { useState } from 'react'
+import { Button, Card, Row, Col } from 'react-materialize'
+
+
+//next add css library ill do materialize this time
+//uses react-materialize 2.5.2
+function DrumMachine() {
+
+
+  return (
+    <div>
+      <h1>Drum Machine</h1>
+      <Button>Test</Button>
+    </div>
+    
+  )
+
+}
+export default DrumMachine
